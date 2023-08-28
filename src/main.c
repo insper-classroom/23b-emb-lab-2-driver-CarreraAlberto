103df16715fa3ea72a1ab4090280a58674894112
CarreraAlberto <albertomcarrera@hotmail.com>
2023-08-28 11:47:55 -0300

    Lab 2 completo

--- src/main.c
+++ src/main.c
@@ -48,6 +48,15 @@
 #define BUT3_PIO_IDX 19
 #define BUT3_PIO_IDX_MASK (1u << BUT3_PIO_IDX)
 
+/*  Default pin configuration (no attribute). */
+#define _PIO_DEFAULT             (0u << 0)
+/*  The internal pin pull-up is active. */
+#define _PIO_PULLUP              (1u << 0)
+/*  The internal glitch filter is active. */
+#define _PIO_DEGLITCH            (1u << 1)
+/*  The internal debouncing filter is active. */
+#define _PIO_DEBOUNCE            (1u << 3)
+
 /************************************************************************/
 /* constants                                                            */
 /************************************************************************/
@@ -69,6 +78,90 @@ void init(void);
 /************************************************************************/
 /* funcoes                                                              */
 /************************************************************************/
+void _pio_set(Pio *p_pio, const uint32_t ul_mask) {
+	p_pio->PIO_SODR = ul_mask;
+}
+
+void _pio_clear(Pio *p_pio, const uint32_t ul_mask) {
+	p_pio->PIO_CODR = ul_mask;
+}
+
+void _pio_pull_up(Pio *p_pio, const uint32_t ul_mask, const uint32_t ul_pull_up_enable) {
+	if (ul_pull_up_enable) {
+		p_pio->PIO_PUER = ul_mask;
+		} 
+	else {
+		p_pio->PIO_PUDR = ul_mask;
+	}
+}
+
+void _pio_set_input(Pio *p_pio, const uint32_t ul_mask, const uint32_t ul_attribute) {
+	pio_disable_interrupt(p_pio, ul_mask);
+	_pio_pull_up(p_pio, ul_mask, ul_attribute & PIO_PULLUP);
+
+	if (ul_attribute & (PIO_DEGLITCH | PIO_DEBOUNCE)) {
+		p_pio->PIO_IFER = ul_mask;
+		} 
+	else {
+		p_pio->PIO_IFDR = ul_mask;
+	}
+
+	// Inputs
+	p_pio->PIO_ODR = ul_mask;
+	p_pio->PIO_PER = ul_mask;
+}
+
+void _pio_set_output(Pio *p_pio, const uint32_t ul_mask, const uint32_t ul_default_level, const uint32_t ul_multidrive_enable,
+	const uint32_t ul_pull_up_enable) 
+{
+	pio_disable_interrupt(p_pio, ul_mask);
+	_pio_pull_up(p_pio, ul_mask, ul_pull_up_enable);
+
+	// Habilita multidrive
+	if (ul_multidrive_enable) {
+		p_pio->PIO_MDER = ul_mask;
+		} 
+	else {
+		p_pio->PIO_MDDR = ul_mask;
+	}
+
+	// Setando
+	if (ul_default_level) {
+		p_pio->PIO_SODR = ul_mask;
+		} 
+	else {
+		p_pio->PIO_CODR = ul_mask;
+	}
+
+	// Outputs
+	p_pio->PIO_OER = ul_mask;
+	p_pio->PIO_PER = ul_mask;
+}
+
+uint32_t _pio_get(Pio *p_pio, const pio_type_t ul_type, const uint32_t ul_mask) {
+	uint32_t _ul_mask;
+
+	if (ul_type == PIO_OUTPUT_0) {
+		_ul_mask = p_pio->PIO_ODSR;
+	}
+	else {
+		_ul_mask = p_pio->PIO_PDSR;
+	}
+	
+	if ((_ul_mask & ul_mask) == 0) {
+		return 0;
+	}
+	else {
+		return 1;
+	}
+}
+
+void _delay_ms(uint32_t ms) {
+	for (volatile uint32_t i = 0; i < ms * 50000; i++) {
+		// Espera por "ms" milisegundo
+	}
+}
+
 
 // Função de inicialização do uC
 void init(void) {
@@ -87,19 +180,19 @@ void init(void) {
   pmc_enable_periph_clk(BUT3_PIO_ID);
 
   // Inicializa PC8 como saída
-  pio_set_output(LED_PIO, LED_PIO_IDX_MASK, 0, 0, 0);
+  _pio_set_output(LED_PIO, LED_PIO_IDX_MASK, 0, 0, 0);
 
-  pio_set_output(LED1_PIO, LED1_PIO_IDX_MASK, 0, 0, 0);
-  pio_set_output(LED2_PIO, LED2_PIO_IDX_MASK, 0, 0, 0);
-  pio_set_output(LED3_PIO, LED3_PIO_IDX_MASK, 0, 0, 0);
+  _pio_set_output(LED1_PIO, LED1_PIO_IDX_MASK, 0, 0, 0);
+  _pio_set_output(LED2_PIO, LED2_PIO_IDX_MASK, 0, 0, 0);
+  _pio_set_output(LED3_PIO, LED3_PIO_IDX_MASK, 0, 0, 0);
 
-  pio_set_input(BUT1_PIO, BUT1_PIO_IDX_MASK, PIO_DEFAULT);
-  pio_set_input(BUT2_PIO, BUT2_PIO_IDX_MASK, PIO_DEFAULT);
-  pio_set_input(BUT3_PIO, BUT3_PIO_IDX_MASK, PIO_DEFAULT);
+  _pio_set_input(BUT1_PIO, BUT1_PIO_IDX_MASK, _PIO_PULLUP | _PIO_DEBOUNCE);
+  _pio_set_input(BUT2_PIO, BUT2_PIO_IDX_MASK, _PIO_PULLUP | _PIO_DEBOUNCE);
+  _pio_set_input(BUT3_PIO, BUT3_PIO_IDX_MASK, _PIO_PULLUP | _PIO_DEBOUNCE);
 
-  pio_pull_up(BUT1_PIO, BUT1_PIO_IDX_MASK, 1);
-  pio_pull_up(BUT2_PIO, BUT2_PIO_IDX_MASK, 1);
-  pio_pull_up(BUT3_PIO, BUT3_PIO_IDX_MASK, 1);
+  _pio_pull_up(BUT1_PIO, BUT1_PIO_IDX_MASK, 1);
+  _pio_pull_up(BUT2_PIO, BUT2_PIO_IDX_MASK, 1);
+  _pio_pull_up(BUT3_PIO, BUT3_PIO_IDX_MASK, 1);
 }
 
 /************************************************************************/
@@ -111,35 +204,35 @@ int main(void) {
   init();
 
   while (1) {
-    if (!pio_get(BUT1_PIO, PIO_INPUT,
+    if (!_pio_get(BUT1_PIO, PIO_INPUT,
                  BUT1_PIO_IDX_MASK)) { // Caso aperte Botao 1
       for (int i = 0; i < 5; i++) {
-        pio_set(LED1_PIO, LED1_PIO_IDX_MASK);
-        delay_ms(200);
-        pio_clear(LED1_PIO, LED1_PIO_IDX_MASK);
-        delay_ms(200);
+        _pio_set(LED1_PIO, LED1_PIO_IDX_MASK);
+        _delay_ms(200);
+        _pio_clear(LED1_PIO, LED1_PIO_IDX_MASK);
+        _delay_ms(200);
       }
-      pio_clear(LED1_PIO, LED1_PIO_IDX_MASK);
+      _pio_clear(LED1_PIO, LED1_PIO_IDX_MASK);
     }
-    if (!pio_get(BUT2_PIO, PIO_INPUT,
+    if (!_pio_get(BUT2_PIO, PIO_INPUT,
                  BUT2_PIO_IDX_MASK)) { // Caso aperte Botao 2
       for (int i = 0; i < 5; i++) {
-        pio_set(LED2_PIO, LED2_PIO_IDX_MASK);
-        delay_ms(200);
-        pio_clear(LED2_PIO, LED2_PIO_IDX_MASK);
-        delay_ms(200);
+        _pio_set(LED2_PIO, LED2_PIO_IDX_MASK);
+        _delay_ms(200);
+        _pio_clear(LED2_PIO, LED2_PIO_IDX_MASK);
+        _delay_ms(200);
       }
-      pio_clear(LED2_PIO, LED2_PIO_IDX_MASK);
+      _pio_clear(LED2_PIO, LED2_PIO_IDX_MASK);
     }
-    if (!pio_get(BUT3_PIO, PIO_INPUT,
+    if (!_pio_get(BUT3_PIO, PIO_INPUT,
                  BUT3_PIO_IDX_MASK)) { // Caso aperte Botao 3
       for (int i = 0; i < 5; i++) {
-        pio_set(LED3_PIO, LED3_PIO_IDX_MASK);
-        delay_ms(200);
-        pio_clear(LED3_PIO, LED3_PIO_IDX_MASK);
-        delay_ms(200);
+        _pio_set(LED3_PIO, LED3_PIO_IDX_MASK);
+        _delay_ms(200);
+        _pio_clear(LED3_PIO, LED3_PIO_IDX_MASK);
+        _delay_ms(200);
       }
-      pio_clear(LED3_PIO, LED3_PIO_IDX_MASK);
+      _pio_clear(LED3_PIO, LED3_PIO_IDX_MASK);
     }
   }
   // nunca devemos chegar aqui!
